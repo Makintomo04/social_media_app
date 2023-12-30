18aaa369d32d9b6861673dde07d7f6449da74d8e
Michael Akintomo <1721809@alumni.brunel.ac.uk>
2023-12-30 07:23:40 +0000

    Layout fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import Layout from '@/hooks/Layout'
+
 import Modal from '@/components/Modal'
 import LoginModal from '@/components/modals/LoginModal'
 import RegisterModal from '@/components/modals/RegisterModal'
@@ -7,6 +7,7 @@ import type { AppProps } from 'next/app'
 import {Toaster} from "react-hot-toast"
 import {SessionProvider} from "next-auth/react"
 import EditModal from '@/components/modals/EditModal'
+import Layout from '@/components/Layout'
 
 
 export default function App({ Component, pageProps }: AppProps) {
